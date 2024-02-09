e908be40d44a82e8b76bdc364796bff36b869686
Riyapatel1224 <riya.patell1204@gmail.com>
2024-02-09 16:18:58 +0530

    oop concepts

---
+++ C++/practice/enum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+enum week {Monday, Tuesday, Wednesday, Thursday, Friday};
+
+
+int main()
+{
+    week day;
+    day = Tuesday;
+    cout << "Day= " << day + 1;
+
+}
---
+++ C++/practice/struct.cpp
@@ -0,0 +1,36 @@
+// #include<iostream>
+// using namespace std;
+
+// struct Rectangle{
+//     int width, height;
+// };
+
+// int main()
+// {
+//     struct Rectangle r;
+//     r.width = 10;
+//     r.height = 10;
+//     cout << "widht * height = " << r.width * r.height;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// struct Rectangle
+// {
+//     int width, height;
+//     Rectangle(int w,int h)
+//     {
+//         width = w;
+//         height = h;
+//     }
+//     void areaOfRectangle(){
+//         cout << "Area: " << width * height;
+//     }
+// };
+
+// int main()
+// {
+//     struct Rectangle rec = Rectangle(4, 6);
+//     rec.areaOfRectangle();
+// }
