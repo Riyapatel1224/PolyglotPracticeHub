645001ff8697aff3245f8e7b6aa9f9c85d8bff04
Riyapatel1224 <riya.patell1204@gmail.com>
2024-02-16 17:13:40 +0530

    shifting to left

--- DSA/array/shifting.c
+++ DSA/array/shifting.c
@@ -2,7 +2,7 @@
 #define n 5
 int main()
 {
-    int ar[n], i, temp, shift, j;
+    int ar[n], i, temp, shift, j,s;
     printf("Enter the elements of array \n");
     for (i = 0; i < 5; i++)
         scanf("%d", &ar[i]);
@@ -14,16 +14,19 @@ int main()
     printf("\nEnter how many times you want to shift array :\n");
     scanf("%d", &shift);
 
-
-    while(shift)
+    while (shift)
     {
-        temp = ar[0];  
-        for (i = 0; i < n-1; i++) 
+        if(shift==5)
         {
-            ar[i] = ar[i + 1];
+            break;
         }
-        ar[n - 1] = temp;
-        shift--;
+            temp = ar[0];
+            for (i = 0; i < n; i++)
+            {
+                ar[i] = ar[i + 1];
+            }
+            ar[n - 1] = temp;
+            shift--;
     }
 
     printf("\nElements after shifting %d positions to right :\n", shift);
@@ -32,3 +35,74 @@ int main()
 
     return 0;
 }
+
+// 1 2 3 4 5
+// 5 1 2 3 4
+
+// temp = ar[0];
+// for (i = 0; i < n; i++)
+// {
+//     ar[i] = ar[i + 1];
+// }
+// ar[n - 1] = temp;
+// shift--;
+
+// temp = ar[n - 1];
+// for (i = n - 1; i > 0; i--)
+// {
+//     ar[i] = ar[i - 1];
+// }
+// ar[0] = temp;
+// shift--;
+
+// #include <stdio.h>
+// #define n 5
+// int main()
+// {
+//     int ar[n], i, temp, shift, j, s;
+//     printf("Enter the elements of array \n");
+//     for (i = 0; i < 5; i++)
+//         scanf("%d", &ar[i]);
+
+//     printf("\nElements in original order are :\n");
+//     for (i = 0; i < n; i++)
+//         printf(" %d ", ar[i]);
+
+//     printf("\nEnter how many times you want to shift array :\n");
+//     scanf("%d", &shift);
+
+//     while (shift)
+//     {
+//         if (shift == 5)
+//         {
+//             break;
+//         }
+//         if (shift > 5)
+//         {
+//             s = shift % (shift - 5);
+//             temp = ar[0];
+//             for (i = 0; i < n; i++)
+//             {
+//                 ar[i] = ar[i + 1];
+//             }
+//             ar[n - 1] = temp;
+//             shift--;
+//         }
+//         else
+//         {
+//             temp = ar[0];
+//             for (i = 0; i < n; i++)
+//             {
+//                 ar[i] = ar[i + 1];
+//             }
+//             ar[n - 1] = temp;
+//             shift--;
+//         }
+//     }
+
+//     printf("\nElements after shifting %d positions to right :\n", shift);
+//     for (i = 0; i < n; i++)
+//         printf(" %d ", ar[i]);
+
+//     return 0;
+// }
