1b475c17be70bbbc66980ee11d3e5780bca4fa84
Riyapatel1224 <riya.patell1204@gmail.com>
2024-02-21 20:34:01 +0530

    form update

--- js/dom/form.js
+++
@@ -1,8 +0,0 @@
-function formSubmit() {
-    var name1 = document.getElementById("name");
-    var email = document.getElementById("email");
-    var lname = document.getElementById("lname");
-
-    output.innerHTML = "Hi: " + name1.value + "<br>" + "email: " + email.value + "<br>" + "last name: " + lname.value;
-
-}
