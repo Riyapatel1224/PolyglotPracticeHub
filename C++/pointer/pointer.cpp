64eb5cc8c2b1aee64743b245fac4816a70ecc7d9
Riyapatel1224 <riya.patell1204@gmail.com>
2024-02-21 14:20:59 +0530

    pointer

---
+++ C++/pointer/pointer.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int change(int i)
+// {
+//     int *p = &i;
+//     *p = 1000;
+//     return i;
+// }
+
+// int test(int a,int b)
+// {
+//     cout << a;
+// }
+
+// int test2(int a ,int b)
+// {
+//     cout << b;
+// }
+
+// void change(int *p)
+// {
+//     //001=1000
+//     *p = 1000;
+// }
+
+// int* returnpointer()
+// {
+//     int a = 10;
+//     int *p = &a;
+//     *p = 200;
+//     return p;
+// }
+
+int main()
+{
+    // int a=10;
+    // int *p = &a;
+
+    // cout << "value : " << *p << endl;
+    // cout << "address : " << p << endl;
+
+    // cout << "value : " << a << endl;
+    // cout << "address : " << &a << endl;
+
+    // p++;
+
+    // cout << "value : " << *p << endl;
+    // cout << "address : " << p << endl;
+
+    // array
+
+    // int a[5] = {1, 2, 3, 4, 5};  // 1 2 3 4 5
+    // int *p = a;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << p[i] << endl;
+    // }
+
+    // cout << p << endl;
+
+    // int a[5] = {1, 2, 3, 4, 5}; // 1 2 3 4 5
+    // int *p = a;
+
+    // for (int i = 0; i < 5;i++)
+    // {
+    //     b[i] = a[i];
+    // }
+
+    // for (int i = 0; i < 5;i++)
+    // {
+    //     cout << p[i] << endl;
+    // }
+
+    // string name = "smit";
+    // string *cpyname = &name;
+
+    // cout << "cpyname: " << *cpyname;
+
+    // char name[10]="smit";
+    // char *p = name;
+
+    // for (int i = 0; i < 4;i++)
+    // {
+    //     cout << "p: " << p[i] << endl;
+    // }
+
+    // function
+
+    // int i = 100;
+    // int j=change(i);
+    // cout << "i: " << j;
+
+    // function to pointer
+
+    // int (*p)(int, int) = test;
+    // int (*p2)(int, int) = test2;
+    // p(10,20);
+    // p2(10,20);
+
+    // pointer to function
+
+    // int x = 10; //001
+    // change(&x); //001
+    // cout<<"value of x is : "<<x;
+
+    // int *p = returnpointer();
+
+    // cout << *p << endl;
+
+    // return 0;
+}
