34099583dcf5ce9f03545666e34960a3a13d5f2c
Riyapatel1224 <riya.patell1204@gmail.com>
2024-03-05 15:24:24 +0530

    queue

--- DSA/array/Queue/queue.c
+++ DSA/array/Queue/queue.c
@@ -25,8 +25,41 @@ int insert(int num)
 
 void display()
 {
-    
+    int i;
+    if(f == -1)
+    {
+        printf("\nQueue is empty\n");
+    }
+    else
+    {
+        for (i = f; i <= r; i++)
+        {
+            printf(" %d", queue[i]);
+        }
+    }
 }
+
+void removeQ()
+{
+    if(f==-1)
+    {
+        printf("\nQueue is Empty\n");
+    }
+    else
+    {
+        printf("\n%d is removed\n",queue[f]);
+        if(f==r)
+        {
+            f = -1;
+            r = -1;
+        }
+        else
+        {
+            f++;
+        }
+    }
+}
+
 
 
 int main()
@@ -34,19 +67,19 @@ int main()
     int choice, num;
     while(1)
     {
-        printf("1.INSERT\n2.DELETE\n3.DISPLAY\n0.EXIT\nENTER YOUR CHOICE: \n");
+        printf("\n1.INSERT\n2.DELETE\n3.DISPLAY\n0.EXIT\nENTER YOUR CHOICE: \n");
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
-            printf("\nENTER A NUMBER TO INSERT: ");
+            printf("\nENTER A NUMBER TO INSERT: \n");
             scanf("%d", &num);
             insert(num);
             break;
 
         case 2:
-            delete();
+            removeQ();
             break;
 
         case 3:
@@ -54,10 +87,10 @@ int main()
             break;
 
         case 0:
-            EXIT(0);
+            exit(1);
 
         default:
-            printf("\nEnter a valid choice PTA");
+            printf("\nEnter a valid choice PTA\n");
             break;
         }
     }
