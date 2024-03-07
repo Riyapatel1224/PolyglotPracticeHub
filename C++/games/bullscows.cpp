9bed7efb122a7fe0dcd89a9b7dddfbd8bedd7615
Riyapatel1224 <riya.patell1204@gmail.com>
2024-03-07 18:18:17 +0530

    bulls and cows

--- C++/games/bullscows.cpp
+++ C++/games/bullscows.cpp
@@ -1,24 +1,59 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int seperate(int gnum)
+void seperate(int gnum, int &i, int &j, int &k)
 {
-    int i;
-    for (i = 0; i < 3;i++)
-    {
-        int temp=0;
-        temp = gnum % 10;
-        cout << temp<<endl;
-        gnum /= 10;
-    }
-    
 
+    // for (i = 0; i < 3;i++)
+    // {
+    //     int temp=0;
+    //     temp = gnum % 10;
+    //     cout << temp<<endl;
+    //     gnum /= 10;
+    // }
+
+    k = gnum % 10;         // 3
+    j = (gnum % 100) / 10; // 2
+    i = gnum / 100;        // 1
+
+    // cout << i << endl;
+    // cout << j << endl;
+    // cout << k << endl;
 }
 
 int main()
 {
-    int snum = 123,gnum, i; // secret number
-    cout << "Enter your guess: "<<endl;
+    int snum = 123, gnum, si, sj, sk, gi, gj, gk, bulls = 0, cows = 0;
+    cout << "Enter your guess: " << endl;
     cin >> gnum;
-    seperate(gnum);
+    seperate(gnum, si, sj, sk);
+    seperate(snum, gi, gj, gk);
+
+    if (si == gi)
+    {
+        bulls++;
+    }
+    if (sj == gj)
+    {
+        bulls++;
+    }
+    if (sk == gk)
+    {
+        bulls++;
+    }
+
+    if ((si == gj || si == gk) && si != gi)
+    {
+        cows++;
+    }
+    if ((sj == gi || sj == gk) && sj != gj)
+    {
+        cows++;
+    }
+    if ((sk == gi || sk == gj) && sk != gk)
+    {
+        cows++;
+    }
+    cout << "bulls : " << bulls << endl;
+    cout << "cows : " << cows << endl;
 }
