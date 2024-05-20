8f4870f8030e63938e036b21ab95da0ab07b7cf2
Riyapatel1224 <riya.patell1204@gmail.com>
2024-05-20 15:54:05 +0530

    JAVA

---
+++ JAVA/temp/src/temp/Demo.java
@@ -0,0 +1,14 @@
+package temp;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		int a =100,b=200;
+		System.out.println("Hello");
+		System.out.println(100);
+		System.out.println("sum is : "+a+b);
+		System.out.println("sum is IS : "+(a+b));
+	}
+	
+}
+
