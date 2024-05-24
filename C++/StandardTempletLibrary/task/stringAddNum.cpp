8c9cc30df1a6ab423cecf0e77d6048060173a0a9
Riyapatel1224 <riya.patell1204@gmail.com>
2024-05-24 15:57:09 +0530

    string concate

--- C++/StandardTempletLibrary/task/stringAddNum.cpp
+++ C++/StandardTempletLibrary/task/stringAddNum.cpp
@@ -1,18 +1,70 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<list>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
 
 using namespace std;
 
-
-    int ArithmeticOp(vector<int> num,vector<char> ch,vector<string> input)
+int ArithmeticOp(vector<int> num, vector<char> ch, vector<string> input)
+{
+    int i, j;
+    for (auto i : input)
     {
-        int i;
-        for(auto i: input)
+        for (auto j : i)
         {
-            cout << index << endl;
+            // cout << " " << j << endl;
+            if (isdigit(j))
+            {
+                // cout << j << endl;
+                num.push_back(j - '0');
+            }
+            else
+            {
+                ch.push_back(j);
+            }
         }
+    }
+    // cout << "Numbers: " << endl;
+    // for (int j : num)
+    // {
+    //     cout << " " << j << endl;
+    // }
+    // cout << "String: " << endl;
+    // for (char i : ch)
+    // {
+    //     cout << " " << i << endl;
+    // }
+    int output;
+
+    cout << "Result : " << output << endl;
+
+        // if (ch[0] == '+')
+    // {
+    //     if(ch[1]=='*'){
+    //         output = num[0] + num[1] * num[2];
+    //     }
+    //     else if(ch[1]=='/'){
+    //         output = num[0] + num[1] / num[2];
+    //     }
+    //     else{
+    //         output = num[0] + num[1] + num[2];
+    //     }
+    // }
+    // else if (ch[0] == '+')
+    // {
+    //     if (ch[1] == '*')
+    //     {
+    //         output = num[0] + num[1] * num[2];
+    //     }
+    //     else if (ch[1] == '/')
+    //     {
+    //         output = num[0] + num[1] / num[2];
+    //     }
+    //     else
+    //     {
+    //         output = num[0] + num[1] + num[2];
+    //     }
+    // }
     }
 
 int main()
