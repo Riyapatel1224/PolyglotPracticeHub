832e43be61a669a5fce57a8fd9fb169b41d58a8a
Riyapatel1224 <riya.patell1204@gmail.com>
2024-06-05 17:57:22 +0530

    BST

--- DSA/Tree/BST.c
+++ DSA/Tree/BST.c
@@ -34,12 +34,91 @@ struct node* addNode(struct node *root,int data){
     return root;
 }
 
+void inOrder(struct node *root){
+    if(root!=NULL){
+        inOrder(root->left);
+        printf(" %d", root->data);
+        inOrder(root->right);
+    }
+}
+
+void preOrder(struct node *root){
+    if(root!=NULL){
+        printf(" %d", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct node *root){
+    if(root!=NULL){
+        preOrder(root->left);
+        preOrder(root->right);
+        printf(" %d", root->data);
+    }
+}
+
+void search(struct node *root, int key)
+{
+    if (root == NULL)
+    {
+        printf("\n%d NotFound", key);
+    }
+    else if (root->data == key)
+    {
+        printf("\n %d Found", key);
+    }
+    else if (key > root->data)
+    {
+        // right
+        search(root->right, key);
+    }
+    else
+    { // left
+        search(root->left, key);
+    }
+}
+
+void max(struct node *root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+    else if (root->right == NULL)
+    {
+        printf(" \n max =  %d", root->data);
+    }
+    else
+    {
+        max(root->right);
+    }
+}
+
 int main()
 {
     struct node *root = NULL;
     root=addNode(root, 20);
     addNode(root, 10);
     addNode(root, 40);
+    addNode(root, 50);
 
-    printf("%d %d %d", root->left->data, root->data, root->right->data);
+    printf("\ninOrder");
+    inOrder(root);
+
+    printf("\npreOrder");
+    preOrder(root);
+
+    printf("\npostOrder");
+    postOrder(root);
+
+    printf("\n SEARCH");
+    search(root, 200);
+
+    printf("\n Max");
+    max(root);
+
+    // l r ro
+    //  printf("\n%d %d %d %d", root->left->data, root->data, root->right->data,root->right->right->data);
 }
